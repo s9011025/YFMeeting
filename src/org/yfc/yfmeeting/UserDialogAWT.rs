5d15773e5a88aeb7ce990aa47c715b4881a29ee4
s9011025 <s9011025@gmail.com>
2018-01-08 23:58:02 +0800

    Delete UserDialogAWT.rs

--- src/org/yfc/yfmeeting/UserDialogAWT.rs
+++
@@ -1,5 +0,0 @@
-org.litesoft.p2pchat.UserDialogAWT$1
-org.litesoft.p2pchat.UserDialogAWT
-org.litesoft.p2pchat.UserDialogAWT$2
-org.litesoft.p2pchat.UserDialogAWT$3
-org.litesoft.p2pchat.UserDialogAWT$4
