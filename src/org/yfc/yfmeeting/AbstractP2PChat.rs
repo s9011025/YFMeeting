bbda87b1daeba150bf2ea631df8cecfaf1b3cf5e
s9011025 <s9011025@gmail.com>
2018-01-08 23:55:08 +0800

    Delete AbstractP2PChat.rs

--- src/org/yfc/yfmeeting/AbstractP2PChat.rs
+++
@@ -1,4 +0,0 @@
-org.litesoft.p2pchat.AbstractP2PChat$ArgsParser$AddressPort
-org.litesoft.p2pchat.AbstractP2PChat$ArgsParser$DottedAddress
-org.litesoft.p2pchat.AbstractP2PChat$ArgsParser
-org.litesoft.p2pchat.AbstractP2PChat
